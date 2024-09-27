db0ceb183cd5b6fb444f0ccff6a84f418fed2f90
DEEPAK-225 <kumarvermadeepak225@gmail.com>
2024-09-27 21:36:16 +0530

    Adding and removing functionality of add to cart button fixed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // Array to hold cart items
 let cartItems = [];
-let itemCount = [];
+let itemCount = []; 
    const emptyCartImage = document.getElementById('emptyCartImage');
     emptyCartImage.innerHTML = '<img class="empty-cart-img" src="assets/images/illustration-empty-cart.svg" alt="emptyCartImage">'
     const emptyCartPara = document.getElementById('emptyCartPara');emptyCartPara.innerText = 'Your added items will appear here';
+
 
 function fetchAndDisplayDesserts() {
     fetch('data.json')
@@ -34,11 +35,11 @@ function fetchAndDisplayDesserts() {
                 image.src = `${dessert.image.thumbnail}`;
                 image.alt = `${dessert.name}`;
                 image.id = 'itemImage';
-                addToCartBtn.innerHTML = '<img src="./assets/images/icon-add-to-cart.svg" alt="Description of icon" width="24" height="24" /> <span>Add To Cart</span>';
+                addToCartBtn.innerHTML = '<img class="cart-icon" src="./assets/images/icon-add-to-cart.svg" alt="Description of icon" width="24" height="24"/> Add To Cart';
                 itemCategory.innerText = `${dessert.category}`;
                 itemName.innerText = `${dessert.name}`;
                 itemPrice.innerText = `$${dessert.price.toFixed(2)}`;
-
+                addToCartBtn.setAttribute('id',`${dessert.name}+1`);
 
                 // Adding respective classes to the elements.
                 dessertItem.classList.add('desserts-item');
@@ -59,16 +60,75 @@ function fetchAndDisplayDesserts() {
                 itemDetails.appendChild(itemName);
                 itemDetails.appendChild(itemPrice);
 
-                // Click event for Add to Cart button for each dessert
+
+                //creating add to cart btn afte first click
+                const svgNS = "http://www.w3.org/2000/svg";
+
+                // Create decrement button SVG
+                const decrementSvg = document.createElementNS(svgNS, "svg");
+                decrementSvg.setAttribute("id", "decrement-quantity");
+                decrementSvg.setAttribute("class", "ind-btn");
+                decrementSvg.setAttribute("width", "24");
+                decrementSvg.setAttribute("height", "24");
+                decrementSvg.setAttribute("viewBox", "0 0 24 24");
+        
+                const decrementPath = document.createElementNS(svgNS, "path");
+                decrementPath.setAttribute("d", "M5 12h14");
+                decrementPath.setAttribute("stroke", "white");
+                decrementPath.setAttribute("stroke-width", "2");
+                decrementPath.setAttribute("fill", "none");
+        
+                decrementSvg.appendChild(decrementPath);
+        
+                // Create increment button SVG
+                const incrementSvg = document.createElementNS(svgNS, "svg");
+                incrementSvg.setAttribute("id", "increment-quantity");
+                incrementSvg.setAttribute("class", "ind-btn");
+                incrementSvg.setAttribute("width", "24");
+                incrementSvg.setAttribute("height", "24");
+                incrementSvg.setAttribute("viewBox", "0 0 24 24");
+        
+                const incrementPath = document.createElementNS(svgNS, "path");
+                incrementPath.setAttribute("d", "M12 5v14m-7-7h14");
+                incrementPath.setAttribute("stroke", "white");
+                incrementPath.setAttribute("stroke-width", "2");
+                incrementPath.setAttribute("fill", "none");
+        
+                incrementSvg.appendChild(incrementPath);
+
+                 // Create count items span
+                const countSpan = document.createElement("span");
+                countSpan.setAttribute("id", "itemCounts");
+                countSpan.setAttribute("class", "count-items");
+                //  countSpan.textContent = "1";  // Initial count
+
+                var x = 0;
                 addToCartBtn.addEventListener('click', () => {
-                    addToCart(dessert); 
+                    if(!x){
+                    addToCartBtn.innerHTML = ' ';
+                    addToCart(dessert,null); 
                     document.getElementById('orderAmmountContainer').style.display = 'block';
-                   
-                  emptyCartImage.innerHTML = ' ';
-                  emptyCartPara.innerText = ' ';
-                  addToCartBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg> hello <img src="./assets/images/icon-add-to-cart.svg" alt="Description of icon" width="24" height="24" />`;
+                    x=2;
+                    emptyCartImage.innerHTML = ' ';
+                    emptyCartPara.innerText = ' ';
+                    addToCartBtn.style.backgroundColor = 'hsl(14, 86%, 42%)';
+                    countSpan.innerText = '1';
+                    countSpan.setAttribute('id',`${dessert.name}`);
+                    addToCartBtn.appendChild(decrementSvg)
+                    addToCartBtn.appendChild(countSpan);
+                    addToCartBtn.appendChild(incrementSvg);
+                }
                 });
+                incrementSvg.addEventListener('click', ()=>{
+                  addToCart(dessert,1); 
+                });
+                decrementSvg.addEventListener('click', ()=>{
+                    addToCart(dessert,-1); 
+                 });
+             
+
             });
+           
         })
         .catch(error => {
             console.error('Error fetching the JSON file:', error);
@@ -79,28 +139,56 @@ function fetchAndDisplayDesserts() {
 let totalOrderPrice = 0;
 
 
-function addToCart(dessert) {
+function addToCart(dessert,id) {
    const dessertPrice = Number(dessert.price);
     
    const cartHeading = document.getElementById('cartHeading');
 
-   cartHeading.innerText = `Your Cart (${itemCount.reduce((accumulator, currentValue) => accumulator + currentValue, 0) + 1})`;
-
     const itemInCart = cartItems.find(item => item.name === dessert.name);
-    totalOrderPrice += dessertPrice;
-    document.getElementById('totalOrderAmmount').innerText = `$${totalOrderPrice.toFixed(2)}`;
+    // totalOrderPrice += dessertPrice;
 
-    if (itemInCart) {
+    if (itemInCart && id==1) {
         for (let i = 0; i < cartItems.length; i++) {
             if (dessert.name === cartItems[i].name) {
                 itemCount[i] += 1;
+                document.getElementById(`${dessert.name}`).innerText = `${itemCount[i]}`;
+                totalOrderPrice += dessertPrice;
                 break;
             }
         }
-    } else {
+    } 
+    else if(itemInCart && id==-1){
+        for (let i = 0; i < cartItems.length; i++) {
+            if (dessert.name === cartItems[i].name) {
+                itemCount[i] -= 1;
+                document.getElementById(`${dessert.name}`).innerText = `${itemCount[i]}`;
+                totalOrderPrice -= dessertPrice;
+                if(itemCount[i]==0){
+                    cartItems.splice(i, 1);
+                document.getElementById(`${dessert.name}+1`).innerHTML ='<img class="cart-icon" src="./assets/images/icon-add-to-cart.svg" alt="Description of icon" width="24" height="24"/> Add To Cart';
+                document.getElementById(`${dessert.name}+1`).style.backgroundColor = 'white';
+                document.getElementById('orderAmmountContainer').style.display = 'none';
+                 emptyCartImage.innerHTML = '<img class="empty-cart-img" src="assets/images/illustration-empty-cart.svg" alt="emptyCartImage">';
+                 emptyCartPara.innerText = 'Your added items will appear here';
+
+                }
+                document.getElementById('totalOrderAmmount').innerText = `$${totalOrderPrice.toFixed(2)-2*dessert.price}`;
+                break;
+            }
+        }
+    }
+    else if(id == null){
         cartItems.push(dessert);
         itemCount.push(1);
+        totalOrderPrice += dessertPrice;
     }
+     let count=0;
+    for(let i=0;i<itemCount.length;i++){
+       count+=itemCount[i];
+    }
+    cartHeading.innerText = `Your Cart (${count})`;
+
+    document.getElementById('totalOrderAmmount').innerText = `${totalOrderPrice.toFixed(2)}`;
 
     const cartItemsContainer = document.getElementById('cartItemsContainer');
     cartItemsContainer.innerHTML = ' ';
@@ -127,6 +215,9 @@ function addToCart(dessert) {
         cartItemTotalPrice.innerText = ` $${itemPrice.toFixed(2)} `;
         cartItemPrice.innerText = ` $${(itemPrice * itemCountValue).toFixed(2)}`;
         removeBtn.innerHTML = '<img src="assets/images/icon-remove-item.svg" alt="remove-item">';
+        removeBtn.style.color = 'hsl(14, 86%, 42%)';
+        removeBtn.setAttribute('class','remove-btn');
+
 
         // Add classes to elements
         cartItem.classList.add('cart-item');
@@ -156,11 +247,15 @@ function addToCart(dessert) {
             totalOrderPrice -= itemPrice * itemCountValue; // Correct price calculation
             totalOrderPrice = isNaN(totalOrderPrice) ? 0 : totalOrderPrice; // Handle NaN case
             document.getElementById('totalOrderAmmount').innerText = `$${totalOrderPrice.toFixed(2)}`;
+
+            document.getElementById(`${dessert.name}+1`).innerHTML ='<img class="cart-icon" src="./assets/images/icon-add-to-cart.svg" alt="Description of icon" width="24" height="24"/> Add To Cart';
+            document.getElementById(`${dessert.name}+1`).style.backgroundColor = 'white';
             
             itemCount.splice(index, 1); // Remove count of item
             cartItems.splice(index, 1); // Remove item from cart
             cartItem.remove(); // Remove item from the DOM
             hr.remove(); // Remove the hr element from the DOM
+
 
             // Update cart heading after item removal
             const totalItemCount = itemCount.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
@@ -173,8 +268,10 @@ function addToCart(dessert) {
                  emptyCartImage.innerHTML = '<img class="empty-cart-img" src="assets/images/illustration-empty-cart.svg" alt="emptyCartImage">';
                  emptyCartPara.innerText = 'Your added items will appear here';
             }
+           
         });
     });
+   
 }
 
 document.getElementById('orderBtn').addEventListener('click', () => {
@@ -215,7 +312,7 @@ function confirmOrder () {
 
     // Set values and innerText
     orederedItemImage.src = `${item.image.thumbnail}`;
-    orderedItem.innerText = `${item.name}`;
+    orderedItemName.innerText = `${item.name}`;
     orderedItemCount.innerText = `${itemCountValue}x `;
     orderedItemPrice.innerText = ` $${itemPrice.toFixed(2)} `;
     orderedItemTotalPrice.innerText = ` $${(itemPrice * itemCountValue).toFixed(2)}`;
